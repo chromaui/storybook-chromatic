8ee85c827ca610dfed790b05a9eaea8035a78b46
Chroma Machine <machine@hichroma.com>
2018-10-08 23:31:55 +0000

    Commit 61c0fb1357a63cf0fe03a236ec497d813a191d60

--- dist/bin/chromatic-test.js
+++ dist/bin/chromatic-test.js
@@ -61,6 +61,7 @@ var executeTest = exports.executeTest = function () {
 // Normal usage, outside of test
 
 
+exports.findOption = findOption;
 exports.parseArgv = parseArgv;
 
 var _commander = require('commander');
@@ -84,8 +85,9 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 // Ensure NODE_ENV is set
 process.env.NODE_ENV = process.env.NODE_ENV || 'test';
 
+// This is not exactly clever but it works most of the time
 function findOption(storybookScript, shortName, longName) {
-  var parts = storybookScript.split(/[\s+|=]/);
+  var parts = storybookScript.split(/[\s='"]+/);
   var index = parts.indexOf(longName);
   if (index === -1) {
     index = parts.indexOf(shortName);
@@ -97,7 +99,7 @@ function findOption(storybookScript, shortName, longName) {
 }
 
 function parseArgv(argv) {
-  var commander = new _commander.Command().option('-a, --app-code [code]', 'the code for your app, get from chromaticqa.com').option('-s, --script-name [name]', 'The npm script that starts your storybook [storybook]').option('-e, --exec [command]', 'Alternatively, a full command to run to start your storybook.').option('-S, --do-not-start', "Don't attempt to start; use if your storybook is already running").option('-p, --storybook-port [port]', 'What port is your Storybook running on (auto detected from -s, if set)?').option('-u, --storybook-url [url]', 'Storybook is already running at url (implies -S)').option('--ci', 'This build is running on CI, non-interactively (alternatively, pass CI=true)').option('--auto-accept-changes', 'Accept any (non-error) changes or new stories for this build').option('--exit-zero-on-changes', "Use a 0 exit code if changes are detected (i.e. don't stop the build)").option('--no-interactive', 'Do not prompt for package.json changes').option('--only [component:story]', 'Only run a single story (for debugging purposes)').option('--debug', 'Output more debugging information')
+  var commander = new _commander.Command().option('-a, --app-code [code]', 'the code for your app, get from chromaticqa.com').option('-s, --script-name [name]', 'The npm script that starts your storybook [storybook]').option('-e, --exec [command]', 'Alternatively, a full command to run to start your storybook.').option('-S, --do-not-start', "Don't attempt to start; use if your storybook is already running").option('-p, --storybook-port [port]', 'What port is your Storybook running on (auto detected from -s, if set)?').option('-u, --storybook-url [url]', 'Storybook is already running at (external) url (implies -S)').option('-d, --storybook-build-dir [dirname]', 'Provide a directory with your built storybook, implies -S').option('--ci', 'This build is running on CI, non-interactively (alternatively, pass CI=true)').option('--auto-accept-changes', 'Accept any (non-error) changes or new stories for this build').option('--exit-zero-on-changes', "Use a 0 exit code if changes are detected (i.e. don't stop the build)").option('--no-interactive', 'Do not prompt for package.json changes').option('--only [component:story]', 'Only run a single story (for debugging purposes)').option('--debug', 'Output more debugging information')
 
   // We keep this for back compat it does nothing (ie. it is the default)
   .option('--storybook-addon', '(deprecated) use the storybook addon').parse(argv);
@@ -110,6 +112,7 @@ function parseArgv(argv) {
     noStart: !!commander.doNotStart,
     port: commander.storybookPort,
     url: commander.storybookUrl,
+    dirname: commander.storybookBuildDir,
     only: commander.only,
     fromCI: !!commander.ci,
     autoAcceptChanges: !!commander.autoAcceptChanges,
@@ -122,8 +125,11 @@ function parseArgv(argv) {
 
   var packageJson = (0, _jsonfile.readFileSync)(_path2.default.resolve('./package.json'));
   var commandName = commanderOptions.commandName;
+  // eslint-disable-next-line prefer-const
+
   var port = commanderOptions.port,
       url = commanderOptions.url,
+      dirname = commanderOptions.dirname,
       noStart = commanderOptions.noStart,
       scriptName = commanderOptions.scriptName;
 
@@ -132,10 +138,15 @@ function parseArgv(argv) {
     throw new Error('Cannot use both --scriptName and --commandName');
   }
 
-  if (url) {
+  if (url || dirname) {
     if (scriptName || commandName) {
-      throw new Error('Cannot use ' + (scriptName ? '--scriptName' : '--exec') + ' with --storybook-url, it implies --do-not-start');
+      throw new Error('Cannot use ' + (scriptName ? '--scriptName' : '--exec') + ' with ' + (url ? '--storybook-url' : '--storybook-directory') + ', it implies --do-not-start');
     }
+
+    if (url && dirname) {
+      throw new Error('Cannot use both --storybook-url and --storybook-directory');
+    }
+
     noStart = true;
   } else {
     if (commandName) {
@@ -160,6 +171,10 @@ function parseArgv(argv) {
     }
 
     url = 'http://localhost:' + port;
+  }
+
+  if (dirname) {
+    return (0, _extends3.default)({}, commanderOptions, { noStart: true, dirname: dirname });
   }
 
   var parsedUrl = new _url.URL(url);
