9c19e442d9ba5c051f7dd7f0776b0b0fd1254884
Chroma Machine <machine@hichroma.com>
2019-01-14 02:13:27 +0000

    Commit 7162aa83784c2ffc063c9fec94e8666e1fedde26

--- dist/assets/configure.js
+++ dist/assets/configure.js
@@ -98,9 +98,10 @@ exports.default = function () {
             if (chromatic) {
               var viewports = chromatic.viewports,
                   delay = chromatic.delay,
-                  disable = chromatic.disable;
+                  disable = chromatic.disable,
+                  noScroll = chromatic.noScroll;
 
-              parameters = (0, _extends3.default)({}, viewports && { viewports: viewports }, delay && { delay: delay }, disable && { disable: disable });
+              parameters = (0, _extends3.default)({}, viewports && { viewports: viewports }, delay && { delay: delay }, disable && { disable: disable }, noScroll && { noScroll: noScroll });
             }
           }
 
