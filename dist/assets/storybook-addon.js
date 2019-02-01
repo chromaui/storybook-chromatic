402d22f91929a2daeab5d6759505859a2c136886
Chroma Machine <machine@hichroma.com>
2019-02-01 06:08:03 +0000

    Commit 33826423bf4e22ac48c870b4049e4b1bd26c3b63

--- dist/assets/storybook-addon.js
+++ dist/assets/storybook-addon.js
@@ -1,75 +1,22 @@
 'use strict';
 
-var _toConsumableArray2 = require('babel-runtime/helpers/toConsumableArray');
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.isChromatic = undefined;
 
-var _toConsumableArray3 = _interopRequireDefault(_toConsumableArray2);
+var _configure = require('./configure');
 
-var _stringify = require('babel-runtime/core-js/json/stringify');
+var _configure2 = _interopRequireDefault(_configure);
 
-var _stringify2 = _interopRequireDefault(_stringify);
+var _isChromatic = require('./isChromatic');
 
-var _client = require('../client');
-
-var _client2 = _interopRequireDefault(_client);
+var _isChromatic2 = _interopRequireDefault(_isChromatic);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-if (!location.pathname.match('/iframe.html')) {
-  console.error('storybook-chromatic should be installed in your `.storybook/config.js`');
-} /* eslint-env browser */
-/* eslint-disable import/no-extraneous-dependencies, global-require */
+(0, _configure2.default)();
 
-var _window = window,
-    __STORYBOOK_CLIENT_API__ = _window.__STORYBOOK_CLIENT_API__,
-    __STORYBOOK_ADDONS_CHANNEL__ = _window.__STORYBOOK_ADDONS_CHANNEL__;
-
-// Simulate Storybook 3.4's API for @storybook/react < 3.4
-
-if (!__STORYBOOK_CLIENT_API__ || !__STORYBOOK_ADDONS_CHANNEL__) {
-  __STORYBOOK_CLIENT_API__ = require('@storybook/react');
-  __STORYBOOK_ADDONS_CHANNEL__ = require('@storybook/addons').default.getChannel();
-}
-
-var runtime = 'storybook';
-(0, _client2.default)({
-  runtime: runtime,
-  renderSpec: function renderSpec(_ref) {
-    var specRuntime = _ref.runtime,
-        input = _ref.input;
-
-    if (specRuntime !== runtime) {
-      throw new Error('Storybook plugin cannot handle ' + specRuntime + ' specs');
-    }
-
-    var kind = input.kind,
-        name = input.name;
-
-    __STORYBOOK_ADDONS_CHANNEL__.emit('setCurrentStory', { kind: kind, story: name });
-
-    return document.getElementById('root');
-  },
-  specs: function specs() {
-    return __STORYBOOK_CLIENT_API__.getStorybook().map(function (_ref2) {
-      var kind = _ref2.kind,
-          stories = _ref2.stories;
-      return stories.map(function (_ref3) {
-        var name = _ref3.name;
-        return {
-          name: name,
-          component: {
-            name: kind,
-            displayName: kind.split('/').slice(-1)[0]
-          },
-          runtime: runtime,
-          input: (0, _stringify2.default)({
-            kind: kind,
-            name: name
-          })
-        };
-      });
-    }).reduce(function (a, b) {
-      return [].concat((0, _toConsumableArray3.default)(a), (0, _toConsumableArray3.default)(b));
-    }, []); // flatten
-  }
-});
+// eslint-disable-next-line import/prefer-default-export
+exports.isChromatic = _isChromatic2.default;
 //# sourceMappingURL=storybook-addon.js.map
