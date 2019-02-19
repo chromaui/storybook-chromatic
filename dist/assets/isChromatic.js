619ad2b20f8a5dc2b3ce90c95eac64def62301d6
Chroma Machine <machine@hichroma.com>
2019-02-19 23:26:04 +0000

    Commit 66cddc4814f58a3830e417615c2210c03174a60f

--- dist/assets/isChromatic.js
+++ dist/assets/isChromatic.js
@@ -3,12 +3,12 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-
-exports.default = function () {
-  return isChromatic;
-};
+exports.default = _default;
 
 /* eslint-env browser */
-
 var isChromatic = window.navigator.userAgent.match(/Chromatic/) || window.location.href.match(/chromatic=true/);
+
+function _default() {
+  return isChromatic;
+}
 //# sourceMappingURL=isChromatic.js.map
