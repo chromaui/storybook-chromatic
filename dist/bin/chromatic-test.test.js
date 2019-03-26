acdba88ba69b43d4e55245272a96c689e24e66d4
Chroma Machine <machine@hichroma.com>
2019-03-26 07:47:19 +0000

    Commit fca65fd4c8d377e8b0674824bae6725816e3144a

--- dist/bin/chromatic-test.test.js
+++ dist/bin/chromatic-test.test.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-var _chromaticTest = require('./chromatic-test');
+var _chromaticTest = require("./chromatic-test");
 
 jest.mock('../assets/environment', function () {
   return {
@@ -8,7 +8,6 @@ jest.mock('../assets/environment', function () {
     CHROMATIC_APP_CODE: 'env-code'
   };
 });
-
 jest.mock('jsonfile', function () {
   return {
     readFileSync: function readFileSync() {
@@ -22,9 +21,7 @@ jest.mock('jsonfile', function () {
     }
   };
 });
-
 process.env.CHROMATIC_APP_CODE = 'test';
-
 it('sets reasonable defaults', function () {
   expect((0, _chromaticTest.parseArgv)(['node', 'chromatic-test', '--app-code', 'cli-code'])).toMatchObject({
     appCode: 'cli-code',
@@ -32,21 +29,19 @@ it('sets reasonable defaults', function () {
     url: 'http://localhost:1337/iframe.html',
     noStart: false,
     fromCI: false,
-    autoAcceptChanges: false,
-    exitZeroOnChanges: false,
+    autoAcceptChanges: undefined,
+    exitZeroOnChanges: undefined,
     interactive: true,
     verbose: false,
     createTunnel: true,
     originalArgv: ['node', 'chromatic-test', '--app-code', 'cli-code']
   });
 });
-
 it('picks up app-code from environment', function () {
   expect((0, _chromaticTest.parseArgv)(['node', 'chromatic-test'])).toMatchObject({
     appCode: 'env-code'
   });
 });
-
 it('allows you to override defaults for boolean options', function () {
   expect((0, _chromaticTest.parseArgv)(['node', 'chromatic-test', '--ci', '--do-not-start', '--auto-accept-changes', '--exit-zero-on-changes', '--debug', '--no-interactive'])).toMatchObject({
     noStart: true,
@@ -58,50 +53,42 @@ it('allows you to override defaults for boolean options', function () {
     createTunnel: true
   });
 });
-
 it('allows you to specify alternate script, still picks up port', function () {
   expect((0, _chromaticTest.parseArgv)(['node', 'chromatic-test', '--script-name', 'otherStorybook'])).toMatchObject({
     scriptName: 'otherStorybook',
     url: 'http://localhost:7070/iframe.html'
   });
 });
-
 it('allows you to specify alternate script, that does not start storybook, if you set port', function () {
   expect((0, _chromaticTest.parseArgv)(['node', 'chromatic-test', '--script-name', 'notStorybook', '--storybook-port', '6060'])).toMatchObject({
     scriptName: 'notStorybook',
     url: 'http://localhost:6060/iframe.html'
   });
 });
-
 it('throws if you try to specify a script name that is not a storybook, if you do NOT set port', function () {
   expect(function () {
     return (0, _chromaticTest.parseArgv)(['node', 'chromatic-test', '--script-name', 'notStorybook']);
   }).toThrow(/must pass a port/);
 });
-
 it('allows you to specify alternate command if you set port', function () {
   expect((0, _chromaticTest.parseArgv)(['node', 'chromatic-test', '--exec', 'storybook-command', '--storybook-port', '6060'])).toMatchObject({
     commandName: 'storybook-command',
     url: 'http://localhost:6060/iframe.html'
   });
 });
-
 it('throws if you try to specify a command name, if you do NOT set port', function () {
   expect(function () {
     return (0, _chromaticTest.parseArgv)(['node', 'chromatic-test', '--exec', 'storybook-command']);
   }).toThrow(/must pass a port/);
 });
-
 it('throws if you try to pass a script or command name and a url', function () {
   expect(function () {
     return (0, _chromaticTest.parseArgv)(['node', 'chromatic-test', '--exec', 'storybook-command', '--storybook-url', 'http://foo.bar']);
   }).toThrow(/Cannot use --exec with --storybook-url/);
-
   expect(function () {
     return (0, _chromaticTest.parseArgv)(['node', 'chromatic-test', '--script-name', 'storybook', '--storybook-url', 'http://foo.bar']);
   }).toThrow(/Cannot use --scriptName with --storybook-url/);
 });
-
 it('allows you to set a URL without path', function () {
   expect((0, _chromaticTest.parseArgv)(['node', 'chromatic-test', '--storybook-url', 'https://google.com'])).toMatchObject({
     noStart: true,
@@ -109,7 +96,6 @@ it('allows you to set a URL without path', function () {
     createTunnel: false
   });
 });
-
 it('allows you to set a URL with a path', function () {
   expect((0, _chromaticTest.parseArgv)(['node', 'chromatic-test', '--storybook-url', 'https://google.com/foo'])).toMatchObject({
     noStart: true,
@@ -117,12 +103,33 @@ it('allows you to set a URL with a path', function () {
     createTunnel: false
   });
 });
-
 it('allows you to set a URL with iframe.html already set', function () {
   expect((0, _chromaticTest.parseArgv)(['node', 'chromatic-test', '--storybook-url', 'https://google.com/iframe.html?param=foo'])).toMatchObject({
     noStart: true,
     url: 'https://google.com/iframe.html?param=foo',
     createTunnel: false
+  });
+});
+describe('findOption', function () {
+  it('handles short names', function () {
+    var port = (0, _chromaticTest.findOption)('start-storybook -p 9001', '-p', '--port');
+    expect(port).toBe('9001');
+  });
+  it('handles long names', function () {
+    var port = (0, _chromaticTest.findOption)('start-storybook --port 9001', '-p', '--port');
+    expect(port).toBe('9001');
+  });
+  it('handles equals', function () {
+    var port = (0, _chromaticTest.findOption)('start-storybook --port=9001', '-p', '--port');
+    expect(port).toBe('9001');
+  });
+  it('handles double space', function () {
+    var port = (0, _chromaticTest.findOption)('start-storybook --port  9001', '-p', '--port');
+    expect(port).toBe('9001');
+  });
+  it('handles complex scripts', function () {
+    var port = (0, _chromaticTest.findOption)("node verify-node-version.js && concurrently --raw --kill-others 'yarn relay --watch' 'start-storybook -s ./public -p 9001'", '-p', '--port');
+    expect(port).toBe('9001');
   });
 });
 //# sourceMappingURL=chromatic-test.test.js.map
