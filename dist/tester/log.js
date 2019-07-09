861b5a7365feb9d1d307b5f6396aa187d7fa5b84
Chroma Machine <machine@hichroma.com>
2019-07-09 00:59:45 +0000

    Commit 0dd8c5546dd476518a02c23517c8cdc1d81d645d

--- dist/tester/log.js
+++ dist/tester/log.js
@@ -1,11 +1,11 @@
-export default function log(msg, { noPrefix = false, level = 'log' } = {}) {
+export default prefix => (msg, { noPrefix = false, level = 'log' } = {}) => {
   if (process.env.DISABLE_LOGGING !== 'true') {
     if (noPrefix) {
       // eslint-disable-next-line no-console
       console[level](msg);
     } else {
       // eslint-disable-next-line no-console
-      console[level](`Chromatic Tester: ${msg}`);
+      console[level](`${prefix}: ${msg}`);
     }
   }
-}
+};
