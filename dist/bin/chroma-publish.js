f31974e14f07eba28b07ba66a84a9bbfe861452a
Chroma Machine <machine@hichroma.com>
2019-07-30 05:05:48 +0000

    Commit 1132968615b173864c6b5eef34418bd29fb9160a

---
+++ dist/bin/chroma-publish.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var _chromaticTest = require("./chromatic-test");
+
+if (require.main === module) {
+  (0, _chromaticTest.executeTest)(process.argv);
+}
+//# sourceMappingURL=chroma-publish.js.map
